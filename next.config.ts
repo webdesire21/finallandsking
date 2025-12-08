6e6bbe9e2e4149bbcf1598be92fdddc3cf08ee2e
Khushiswami <khushipanthi2002@gmail.com>
2025-12-08 10:48:26 +0530

    madhu

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
   images: {
     domains: ["res.cloudinary.com"],
   },
